f62cb8165d7376148311985c85e6749076e6b5d8
Travis CI User <travis@example.org>
2018-03-15 13:16:59 +0000

    Update documentation

---
+++ implementors/alloc/allocator/trait.Alloc.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["wee_alloc"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/allocator/trait.Alloc.html\" title=\"trait alloc::allocator::Alloc\">Alloc</a> for &amp;'a <a class=\"struct\" href=\"wee_alloc/struct.WeeAlloc.html\" title=\"struct wee_alloc::WeeAlloc\">WeeAlloc</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,13 @@
+(function() {var implementors = {};
+implementors["bigint"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"bigint/uint/enum.FromDecStrErr.html\" title=\"enum bigint::uint::FromDecStrErr\">FromDecStrErr</a>",];
+implementors["byteorder"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"byteorder/enum.BigEndian.html\" title=\"enum byteorder::BigEndian\">BigEndian</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"byteorder/enum.LittleEndian.html\" title=\"enum byteorder::LittleEndian\">LittleEndian</a>",];
+implementors["pwasm_ethereum"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"pwasm_ethereum/struct.Error.html\" title=\"struct pwasm_ethereum::Error\">Error</a>",];
+implementors["rustc_hex"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"rustc_hex/enum.FromHexError.html\" title=\"enum rustc_hex::FromHexError\">FromHexError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
